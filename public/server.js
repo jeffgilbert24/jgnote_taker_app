4e9cae1e3630bac284941bbb9452cef522e99e20
IBBear <jgilbert868@gmail.com>
2021-07-26 20:49:07 -0700

    this works, it needs heroku

--- public/server.js
+++ public/server.js
@@ -9,22 +9,22 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/'))
+);
 
 // required get notes returns notes.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '/notes.html'));
 });
 //required get all returns index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 //required get/api/notes reads the db.json
 app.get('/api/notes', (req, res) => {
-    res.readFile(path.join(__dirname,'./db.json'))
+    res.readFile(path.join(__dirname,'/db.json'))
 });
 
 //post/api/notes recieves a new note to save
