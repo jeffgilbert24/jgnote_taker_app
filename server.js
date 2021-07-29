0819bd37505ea2db7e0c2b723a45a05df03a11e1
IBBear <jgilbert868@gmail.com>
2021-07-29 16:02:18 -0700

    It works

--- server.js
+++ server.js
@@ -1,52 +1,48 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
-
-const app = express()
+const uuid = require('uuid');
+const app = express();
 
 
 const PORT = process.env.PORT || 3001;
-app.use(express.static('./public'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 
 app.get('/', (req, res) =>{
-  res.sendFile(path.join(__dirname, './public/index.html/'))
+  res.sendFile(path.join(__dirname + "/public", 'index.html'));
 });
 
 // required get notes returns notes.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname + "/public", 'notes.html'));
 });
-//required get all returns index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+
 
 //required get/api/notes reads the db.json
-app.get('./api/notes', (req, res) => {
-    fs.readFile(__dirname,'./db/db.json',(err,data)=>{
-        var json = JSON.parse(data);
-        return res.json(json);
+app.get('/api/notes', (req, res) => {
+    const note = JSON.parse(fs.readFileSync('./db/db.json',"utf8"))
+    res.json(note)
+      
     })
+
+  //required get all returns index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + "/public", 'index.html'));
+});  
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
 });
 
-//post/api/notes recieves a new note to save
-app.post("./api/notes/",(req, res) => {
-    newNote = req.body;
-  
-    // Get the JSON file from /db/ and parse it so we can add to it
-    fs.readFile(__dirname + "/db/db.json", (err, data) => {
-      var json = JSON.parse(data);
-      // Push our new note in from our user's request.
-      json.push(newNote);
-  
-      // Write the JSON file over with our new contents.
-      fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(json));
-    });
-  });
+
 
 
 
@@ -54,4 +50,19 @@ app.post("./api/notes/",(req, res) => {
 
 app.listen(PORT, function() {
   console.log("App listening on port "+ PORT);
+});
+
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+
+    newNote["id"] = currentID +1;
+    currentID++;
+    console.log(newNote);
+
+    notes.push(newNote);
+
+    rewriteNotes();
+
+    return res.status(200).end();
 });
